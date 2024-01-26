31fb1716f4a62f9f7344df72859c38e33bcb7a48
renzo <renzofalconi22@hotmail.com>
2024-01-26 10:43:41 -0500

    StyledComponent dependency

--- prueba/pedidos.jsx
+++
@@ -1,9 +0,0 @@
-import Create_order from './create_order'
-
-const Order = () => {
-  return (
-    Create_order()
-  );
-};
-
-export default Order;
--- src/component/FilterList/filterList.jsx
+++ src/component/FilterList/filterList.jsx
@@ -1,20 +1,22 @@
-import {Filter_buttons} from '../../Data/data'
+import { Filter_buttons } from "../../Data/data";
 
 export const FilterList = ({ filterSelected, onFilterChange }) => {
-    return (
-      <div>
-          <ul>
-            {
-              Object.entries(Filter_buttons).map(([key, { label, value }]) => {
-                return (
-                  <li  key={key} onClick={() => { onFilterChange(value);}}>
-                    {label}
-                  </li>
-                );
-              })
-            }
-          </ul>
-      </div>
-    );
-  };
-  
+  return (
+    <div>
+      <ul>
+        {Object.entries(Filter_buttons).map(([key, { label, value }]) => {
+          return (
+            <li
+              key={key}
+              onClick={() => {
+                onFilterChange(value);
+              }}
+            >
+              {label}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
