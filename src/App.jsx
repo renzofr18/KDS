c9a45954c7e5a1750860f41bd4d4bf54fa7d4d1d
renzo <renzofalconi22@hotmail.com>
2024-05-24 00:59:33 -0500

    poner el input

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { OrderList } from './component/OrderList/orderList'
+import {H1} from './globalStyles'
+
+function App() {
+  return (
+    <div>
+    <H1>KITCHEN DISPLAY SYSTEM</H1>
+      <OrderList />
+    </div>
+  )
+}
+
+export default App
